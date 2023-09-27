0ad5c94116961b1cf154a5a7cf5c031f5c25ecc4
Chris <creedrogers@gmail.com>
2023-09-27 06:53:00 -0400

    esc also closes application

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,8 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            if (event.type == sf::Event::Closed) window.close();
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) window.close();
         }
 
         window.clear();
