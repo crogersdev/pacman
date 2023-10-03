730c1ad59ae720b212b168bf367278e30662efd6
Chris <creedrogers@gmail.com>
2023-10-02 22:47:15 -0400

    can move a yellow circle now

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,26 @@
 #include <SFML/Graphics.hpp>
 
 #include <memory>
+#include <functional>
+#include <unordered_map>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(500, 720), "Pacman!");
+
+    sf::CircleShape pacman(30.0f);
+    pacman.setFillColor(sf::Color::Yellow);
+
+    sf::Clock gameClock;
+    sf::Time deltaTime;
+    float movementSpeed = 200.0f;
+
+    std::unordered_map<sf::Keyboard::Key, std::function<void()>> keyActions = {
+        {sf::Keyboard::Left,  [&]() { pacman.move(-movementSpeed * deltaTime.asSeconds(), 0); }},
+        {sf::Keyboard::Right, [&]() { pacman.move( movementSpeed * deltaTime.asSeconds(), 0); }},
+        {sf::Keyboard::Up,    [&]() { pacman.move(0, -movementSpeed * deltaTime.asSeconds()); }},
+        {sf::Keyboard::Down,  [&]() { pacman.move(0,  movementSpeed * deltaTime.asSeconds()); }}
+    };
 
     while (window.isOpen())
     {
@@ -15,10 +29,21 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+                window.close();
+            }
+        }
+
+        deltaTime = gameClock.restart();
+
+        for (const auto& pair : keyActions) {
+            if (sf::Keyboard::isKeyPressed(pair.first)) {
+                pair.second();
+            }
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(pacman);
         window.display();
     }
 
