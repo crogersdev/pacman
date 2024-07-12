32a43e4190ff094b0e8cbf9b260272ba509c3d0a
Chris Rogers <creedrogers@gmail.com>
2024-07-11 23:50:55 -0400

    foo (#10)

--- src/entities/Ghosts.hpp
+++ src/entities/Ghosts.hpp
@@ -8,31 +8,6 @@
 
 #include <SFML/Graphics.hpp>
 
-/*
-
-frontier = PriorityQueue()
-frontier.put(start, 0)
-came_from = dict()
-cost_so_far = dict()
-came_from[start] = None
-cost_so_far[start] = 0
-
-while not frontier.empty():
-   current = frontier.get()
-
-   if current == goal:
-      break
-   
-   for next in graph.neighbors(current):
-      new_cost = cost_so_far[current] + graph.cost(current, next)
-      if next not in cost_so_far or new_cost < cost_so_far[next]:
-         cost_so_far[next] = new_cost
-         priority = new_cost + heuristic(goal, next)
-         frontier.put(next, priority)
-         came_from[next] = current 
-
-*/
-
 class Ghost {
 public:
   // ctors with one callable arg should be marked explicit
@@ -70,7 +45,7 @@ private:
   float mSpeed;
   sf::RectangleShape mGhostShape;
   sf::Vector2f mMovement;
-  sf::Vector2f mPosition;
+  sf::Vector2f mInitialPosition;
   Direction mDirection;
   std::shared_ptr<sf::RenderWindow> mPGameWindow;
   State mState;
@@ -86,7 +61,7 @@ private:
 
   struct OrderByScore {
     bool operator() (const TileScore &a, const TileScore &b) {
-      return a.score < b.score;
+      return a.score > b.score;
     }
   };
 
