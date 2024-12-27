8151791ffb4f135b8196707b500ebfbd9bf0e934
crogersdev <creedrogers@gmail.com>
2024-12-27 14:15:43 -0500

    foo

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,18 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
-#include <iostream>
-#include <memory>
-#include <functional>
-#include <unordered_map>
+int main()
+{
+    sf::Window window(sf::VideoMode({300, 600}), "My window");
 
-#include "GameManager.hpp"
-
-int main() {
-  auto height = TILE_SIZE * LABYRINTH_ROWS + 6 * TILE_SIZE;
-  auto width  = TILE_SIZE * (LABYRINTH_COLS - 1) + 400;  // extra 400 to print out some info
-
-  auto window = std::make_shared<sf::RenderWindow>(sf::VideoMode(sf::Vector2u(width, height)), "Pacman!");
-  GameManager gameManager = GameManager(window);
-
-  while (window->isOpen()) {
-    //gameManager.handleInputs();
-
-    if (!gameManager.mPaused) {
-      gameManager.updateEntities();
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        while (const std::optional event = window.pollEvent())
+        {
+            // "close requested" event: we close the window
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
     }
-
-    gameManager.updateWindow();
-  }
-
-  return 0;
 }
